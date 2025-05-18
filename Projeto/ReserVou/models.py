992a09f2577731e8dc149cedee076cafbb0db92d
GAS097 <work.gabriel.as@gmail.com>
2025-05-18 13:59:10 -0300

    Criado o projeto e os models iniciais

---
+++ Projeto/ReserVou/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+
+class Hotel (models.Model):
+    nome = models.CharField(max_length = 100, null = False, blank = False)
+    endereco = models.CharField(max_length = 150, null = False, blank = False)
+    
+    def qtd_quartos_disponiveis(self):
+        return self.quarto.filter(status='disponivel').count()
+    
+    def __str__(self):
+        return self.nome
+    
+class Quarto (models.Model):
+    STATUS_ATUAL = [
+        ('disponível', 'Disponível'),
+        ('reservado', 'Reservado'),
+    ]
+    
+    numero = models.CharField(max_length = 10, null = False, blank = False)
+    hotel = models.ForeignKey(Hotel, on_delete = models.CASCADE, related_name = 'quarto')
+    tipo = models.CharField(max_length = 20, null = False, blank = False)
+    preco_diaria = models.DecimalField(max_digits = 8, decimal_places = 2, null = False, blank = False)
+    status = models.CharField(max_length = 20, choices = STATUS_ATUAL, default = 'Disponível')    
+    
+    def __str__(self):
+        return f"Quarto {self.numero} - {self.hotel.nome}"
+    
+class Cliente (models.Model):
+    nome = models.CharField(max_length = 100, null = False, blank = False)
+    email = models.EmailField(unique = True, max_length = 100, null = False, blank = False)
+    telefone = models.CharField(max_length = 11, null = False, blank = False)
+    
+    def __str__(self):
+        return self.nome        
+    
+class Reserva (models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE, related_name = 'reservas')
+    hotel = models.ForeignKey(Hotel, on_delete = models.CASCADE, related_name = 'reservas')
+    quarto = models.ForeignKey(Quarto, on_delete = models.CASCADE, related_name = 'reservas')
+    check_in = models.DateField()
+    check_out = models.DateField()
+    
+    def __str__(self):
+        return f"{self.cliente.nome} - {self.hotel.nome} - Quarto {self.quarto.numero} ({self.data_entrada} a {self.data_saida})"
+    
+class Pagamento (models.Model):
+    METODO_ESCOLHIDO = [
+        ('pix', 'Pix'),
+        ('boleto', 'Boleto'),
+        ('credito', 'Cartão de Crédito'),
+        ('debito', 'Cartão de Débito'),        
+    ]
+    
+    reserva = models.OneToOneField(Reserva, on_delete = models.CASCADE, related_name = 'pagamento')
+    valor = models.DecimalField(max_digits = 10, decimal_places = 2)
+    metodo = models.CharField(max_length = 20, choices = METODO_ESCOLHIDO)
+    status = models.CharField(max_length = 20, choices = [
+        ('pendente', 'Pendente'),
+        ('pago', 'Pago'),
+        ('cancelado', 'Cancelado'),
+    ], default = 'pendente')
+    
+    def __str__(self):
+        return f"Pagamento da Reserva {self.reserva.id} - {self.status}"
